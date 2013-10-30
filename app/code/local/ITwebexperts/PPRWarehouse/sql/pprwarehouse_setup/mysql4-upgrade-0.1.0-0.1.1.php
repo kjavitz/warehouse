23db55719a7c14c27c851fa7828f06863127ca62
cristian <cristian@itwebexperts.com>
2013-10-30 15:01:12 +0200

    first

--- app/code/local/ITwebexperts/PPRWarehouse/sql/pprwarehouse_setup/mysql4-upgrade-0.1.0-0.1.1.php
+++ app/code/local/ITwebexperts/PPRWarehouse/sql/pprwarehouse_setup/mysql4-upgrade-0.1.0-0.1.1.php
@@ -3,7 +3,6 @@
 $installer = $this;
 
 $installer->startSetup();
-
 $installer->updateAttribute(Mage_Catalog_Model_Product::ENTITY, 'allow_overbooking', 'is_visible', false);
 /*somehow I had to execute this manually, not sure why needs more testing*/
 $installer->endSetup();
