1ed85392efa2e5e7cb61023a7d7d8fe90502b788
Den4ik <dv.kopilov@gmail.com>
2014-02-28 20:13:47 +0400

    Update to 1.2.0.11

--- app/code/local/ITwebexperts/PPRWarehouse/Model/Payperrentals/Observer.php
+++ app/code/local/ITwebexperts/PPRWarehouse/Model/Payperrentals/Observer.php
@@ -1,69 +1,66 @@
 <?php
+
 /**
  *
  * @author Enrique Piatti
- */ 
+ */
 class ITwebexperts_PPRWarehouse_Model_Payperrentals_Observer extends ITwebexperts_Payperrentals_Model_Observer
 {
 
-	/**
-	 * rewrite for adding stock_id to returns
-	 * (this won't be needed after the PPR refactoring, because the returns will have a reference to the reservationorder table)
-	 * and anyway, this code is repeated also in ITwebexperts_Payperrentals_Adminhtml_AjaxController::sendSelectedAction so we need at leas to follow the DRY principle!
-	 * @param $observer
-	 */
+    /**
+     * rewrite for adding stock_id to returns
+     * (this won't be needed after the PPR refactoring, because the returns will have a reference to the reservationorder table)
+     * and anyway, this code is repeated also in ITwebexperts_Payperrentals_Adminhtml_AjaxController::sendSelectedAction so we need at leas to follow the DRY principle!
+     * @param $observer
+     */
     /*updated*/
-	public function salesOrderShipmentSaveBefore($observer)
-	{
-		// $shipmentData = Mage::app()->getRequest()->getParam('shipment');
-		/** @var Mage_Sales_Model_Order_Shipment $shipment */
-		$shipment = $observer->getEvent()->getShipment();
-		$shippedItems = $shipment->getAllItems();
-		$order = Mage::getModel('sales/order')->load($shipment->getOrderId());
+    public function salesOrderShipmentSaveBefore(Varien_Event_Observer $observer)
+    {
+        // $shipmentData = Mage::app()->getRequest()->getParam('shipment');
+        /** @var Mage_Sales_Model_Order_Shipment $shipment */
+        $shipment = $observer->getEvent()->getShipment();
+        $shippedItems = $shipment->getAllItems();
+        $order = Mage::getModel('sales/order')->load($shipment->getOrderId());
 
-		$sns = Mage::app()->getRequest()->getParam('sn', array());
+        $sns = Mage::app()->getRequest()->getParam('sn', array());
 
-		$reservations = Mage::getModel('payperrentals/reservationorders')
-			->getCollection()
-			->addSelectFilter("order_id = '".$order->getId()."'")
-			->addSelectFilter("product_type = '".ITwebexperts_Payperrentals_Helper_Data::PRODUCT_TYPE."'");
+        $reservations = Mage::getModel('payperrentals/reservationorders')
+            ->getCollection()
+            ->addSelectFilter("order_id = '" . $order->getId() . "'")
+            ->addSelectFilter("product_type = '" . ITwebexperts_Payperrentals_Helper_Data::PRODUCT_TYPE . "'");
 
-		// $reservations->addSendReturnFilter();		// TODO: why it was adding this filter originally? we should be able to have more than one sendreturn for the same reservation right?
+        // $reservations->addSendReturnFilter();		// TODO: why it was adding this filter originally? we should be able to have more than one sendreturn for the same reservation right?
 
-		// check if serial numbers are valid first (throw Exception if not)
-		$dataToSave = array();
-		foreach($reservations as $resOrder)
-		{
-			$orderItemId = $resOrder->getOrderItemId();
-			foreach($shippedItems as $shippedItem)
-			{
-				/** @var $shippedItem Mage_Sales_Model_Order_Shipment_Item */
-				if($shippedItem->getOrderItemId() == $orderItemId)
-				{
-					$dataItemToSave = array(
-						'reservation' => $resOrder,
-						'shipment_item' => $shippedItem,
-						'serial' => array()
-					);
-					$shippedQty = $shippedItem->getQty();
-					/* @var $product Mage_Catalog_Model_Product */
-					$product = Mage::getModel('catalog/product')->load($resOrder->getProductId());
-					$sn = array();
-					$useSerialNumbers = $product->getPayperrentalsUseSerials();
-					if( $useSerialNumbers )
-					{
-						if( isset($sns[$orderItemId]) ){
-							foreach($sns[$orderItemId] as $serial){
-								$serial = trim($serial);
-								if($serial){
-									//TODO check if serial exists and has status A
-									$sn[] = $serial;
-								}
-							}
-						}
-						// what's the idea of this? are we adding the rest of the serial numbers automatically?
-						// why are we entering the serial numbers in first place then?
-						// what happens with partial shipment?
+        // check if serial numbers are valid first (throw Exception if not)
+        $dataToSave = array();
+        foreach ($reservations as $resOrder) {
+            $orderItemId = $resOrder->getOrderItemId();
+            foreach ($shippedItems as $shippedItem) {
+                /** @var $shippedItem Mage_Sales_Model_Order_Shipment_Item */
+                if ($shippedItem->getOrderItemId() == $orderItemId) {
+                    $dataItemToSave = array(
+                        'reservation' => $resOrder,
+                        'shipment_item' => $shippedItem,
+                        'serial' => array()
+                    );
+                    $shippedQty = $shippedItem->getQty();
+                    /* @var $product Mage_Catalog_Model_Product */
+                    $product = Mage::getModel('catalog/product')->load($resOrder->getProductId());
+                    $sn = array();
+                    $useSerialNumbers = $product->getPayperrentalsUseSerials();
+                    if ($useSerialNumbers) {
+                        if (isset($sns[$orderItemId])) {
+                            foreach ($sns[$orderItemId] as $serial) {
+                                $serial = trim($serial);
+                                if ($serial) {
+                                    //TODO check if serial exists and has status A
+                                    $sn[] = $serial;
+                                }
+                            }
+                        }
+                        // what's the idea of this? are we adding the rest of the serial numbers automatically?
+                        // why are we entering the serial numbers in first place then?
+                        // what happens with partial shipment?
 //				if(count($sn) < $resOrder->getQty()){
 //					$coll = Mage::getModel('payperrentals/serialnumbers')
 //						->getCollection()
@@ -79,84 +76,81 @@ class ITwebexperts_PPRWarehouse_Model_Payperrentals_Observer extends ITwebexpert
 //					}
 //				}
 
-						$countBeforeRemovingDuplicates = count($sn);
-						$sn = array_unique($sn);		// make sure there are not duplicated serial numbers
-						if(count($sn) != $countBeforeRemovingDuplicates){
-							Mage::throwException('There are duplicate serial numbers for the product '. $product->getName());
-						}
-						if(count($sn) != $shippedQty){
-							Mage::throwException('Shipped Qty for item '. $product->getName(). ' should be equals to the serial numbers assigned to it');
-						}
+                        $countBeforeRemovingDuplicates = count($sn);
+                        $sn = array_unique($sn); // make sure there are not duplicated serial numbers
+                        if (count($sn) != $countBeforeRemovingDuplicates) {
+                            Mage::throwException('There are duplicate serial numbers for the product ' . $product->getName());
+                        }
+                        if (count($sn) != $shippedQty) {
+                            Mage::throwException('Shipped Qty for item ' . $product->getName() . ' should be equals to the serial numbers assigned to it');
+                        }
 
-						$dataItemToSave['serials'] = $sn;
+                        $dataItemToSave['serials'] = $sn;
 
-					}
-					$dataToSave[] = $dataItemToSave;
+                    }
+                    $dataToSave[] = $dataItemToSave;
 
-					break;
-				}
-			}
-		}
+                    break;
+                }
+            }
+        }
 
-		// we are saving everything at the end so we are sure all the items are OK before saving any of them
-		// TODO: do this inside a transaction
-		foreach($dataToSave as $dataItemToSave)
-		{
-			/** @var $resOrder ITwebexperts_Payperrentals_Model_Reservationorders */
-			$resOrder = $dataItemToSave['reservation'];
-			$shippedItem = $dataItemToSave['shipment_item'];
-			$serials = $dataItemToSave['serials'];
-			$shippedQty = $shippedItem->getQty();
-			$resOrderId = $resOrder->getId();
-			$productId = $resOrder->getProductId();
-			$stockId = $resOrder->getStockId();
-			foreach($serials as $serial)
-			{
-				// TODO: optimize this (for example using only an UPDATE for all the serial numbers and products)
-				$serialModel = Mage::getResourceModel('payperrentals/serialnumbers_collection')
-					->addFieldToFilter('entity_id', $productId)
-					->addFieldToFilter('stock_id', $stockId)
-					->addFieldToFilter('sn', $serial)
-					->getFirstItem();
+        // we are saving everything at the end so we are sure all the items are OK before saving any of them
+        // TODO: do this inside a transaction
+        foreach ($dataToSave as $dataItemToSave) {
+            /** @var $resOrder ITwebexperts_Payperrentals_Model_Reservationorders */
+            $resOrder = $dataItemToSave['reservation'];
+            $shippedItem = $dataItemToSave['shipment_item'];
+            $serials = $dataItemToSave['serials'];
+            $shippedQty = $shippedItem->getQty();
+            $resOrderId = $resOrder->getId();
+            $productId = $resOrder->getProductId();
+            $stockId = $resOrder->getStockId();
+            foreach ($serials as $serial) {
+                // TODO: optimize this (for example using only an UPDATE for all the serial numbers and products)
+                $serialModel = Mage::getResourceModel('payperrentals/serialnumbers_collection')
+                    ->addFieldToFilter('entity_id', $productId)
+                    ->addFieldToFilter('stock_id', $stockId)
+                    ->addFieldToFilter('sn', $serial)
+                    ->getFirstItem();
 
-				if($serialModel->getId()){
-					$serialModel->setStatus('O')->save();
-				}
-				else {
-					// TODO: what happens if we cannot find the serial number? (this could be because the admin used a wrong serial number for example)
-				}
-			}
+                if ($serialModel->getId()) {
+                    $serialModel->setStatus('O')->save();
+                } else {
+                    // TODO: what happens if we cannot find the serial number? (this could be because the admin used a wrong serial number for example)
+                }
+            }
 
-			$serialNumber = implode(',',$serials);
-			$sendReturn = Mage::getModel('payperrentals/sendreturn')
-				->setOrderId($resOrder->getOrderId())
-				->setProductId($productId)
-				->setResStartdate($resOrder->getStartDate())
-				->setResEnddate($resOrder->getEndDate())
-				->setSendDate(date('Y-m-d H:i:s', Mage::getModel('core/date')->timestamp(time())))
-				->setReturnDate('0000-00-00 00:00:00')
-				// ->setQty($resOrder->getQty())//here needs a check this should always be true (TODO: what is this comment?)
-				->setQty($shippedQty)
-				->setSn($serialNumber)
-				->setStockId($stockId)
-				->save();
+            $serialNumber = implode(',', $serials);
+            $sendReturn = Mage::getModel('payperrentals/sendreturn')
+                ->setOrderId($resOrder->getOrderId())
+                ->setProductId($productId)
+                ->setResStartdate($resOrder->getStartDate())
+                ->setResEnddate($resOrder->getEndDate())
+                ->setSendDate(date('Y-m-d H:i:s', Mage::getModel('core/date')->timestamp(time())))
+                ->setReturnDate('0000-00-00 00:00:00')
+                // ->setQty($resOrder->getQty())//here needs a check this should always be true (TODO: what is this comment?)
+                ->setQty($shippedQty)
+                ->setSn($serialNumber)
+                ->setStockId($stockId)
+                ->save();
 
-			// TODO: we should not do this! the FK is wrong (where at really there isn't any FK!), reservations HAS sendreturns, then the FK should be on the return table!
-			Mage::getResourceSingleton('payperrentals/reservationorders')->updateSendReturnById($resOrderId, $sendReturn->getId());
-			//ITwebexperts_Payperrentals_Helper_Data::sendEmail('send', $sendReturn->getId());
-		}
+            // TODO: we should not do this! the FK is wrong (where at really there isn't any FK!), reservations HAS sendreturns, then the FK should be on the return table!
+            Mage::getResourceSingleton('payperrentals/reservationorders')->updateSendReturnById($resOrderId, $sendReturn->getId());
+            //ITwebexperts_Payperrentals_Helper_Data::sendEmail('send', $sendReturn->getId());
+        }
 
-	}
+    }
 
 
 
-	/**
-	 * override for saving the stock_id for the reservationorder, and using the correct qty for the stock instead of getPayperrentalsQuantity
-	 * this should be checked too because we have a very denormalized table right now, why we have a qty and also a reference to the quote/order item?
-	 * in which case the qty from the quote/order_item is different?
-	 * @param $event
-	 * @return $this
-	 */
+    /**
+     * override for saving the stock_id for the reservationorder, and using the correct qty for the stock instead of getPayperrentalsQuantity
+     * this should be checked too because we have a very denormalized table right now, why we have a qty and also a reference to the quote/order item?
+     * in which case the qty from the quote/order_item is different?
+     * @param $event
+     * @return $this
+     */
     /*updated*/
     public function updateCartReservation($event)
     {
@@ -538,46 +532,46 @@ class ITwebexperts_PPRWarehouse_Model_Payperrentals_Observer extends ITwebexpert
     }
 
 
+    public function createOrderBeforeSave($eventData)
+    {
+        $updateItems = array();
+        if ($eventData['request_model']->getPost('item')) {
+            $updateItems = $eventData['request_model']->getPost('item');
+        }
+        $quoteObject = $eventData['order_create_model']->getQuote();
+        if (is_object($quoteObject)) {
+            foreach ($quoteObject->getAllItems() as $item) {
+                if ($item->getProductType() == ITwebexperts_Payperrentals_Helper_Data::PRODUCT_TYPE || $item->getProductType() == ITwebexperts_Payperrentals_Helper_Data::PRODUCT_TYPE_CONFIGURABLE) {
+                    $buyRequest = $item->getBuyRequest();
+                    if (isset($updateItems[$item->getItemId()])) {
+                        $updateItemsArr = $updateItems[$item->getItemId()];
+                    }
 
-	public function createOrderBeforeSave($eventData){
-		$updateItems = array();
-		if($eventData['request_model']->getPost('item')){
-			$updateItems = $eventData['request_model']->getPost('item');
-		}
-		$quoteObject = $eventData['order_create_model']->getQuote();
-		if(is_object($quoteObject)){
-			foreach($quoteObject->getAllItems() as $item){
-				if($item->getProductType() == ITwebexperts_Payperrentals_Helper_Data::PRODUCT_TYPE || $item->getProductType() == ITwebexperts_Payperrentals_Helper_Data::PRODUCT_TYPE_CONFIGURABLE){
-					$buyRequest = $item->getBuyRequest();
-					if(isset($updateItems[$item->getItemId()])){
-						$updateItemsArr = $updateItems[$item->getItemId()];
-					}
-
-					if(isset($updateItemsArr['qty'])){
-						$qty = $updateItemsArr['qty'];
-					}else{
-						$qty = $buyRequest->getQty();
-					}
-					if(isset($updateItemsArr['start_date'])){
-						$start_date = $updateItemsArr['start_date'];
-					}else{
-						$start_date = $buyRequest->getStartDate();
-					}
-					if(isset($updateItemsArr['end_date'])){
-						$end_date = $updateItemsArr['end_date'];
-					} else{
-						$end_date = $buyRequest->getEndDate();
-					}
+                    if (isset($updateItemsArr['qty'])) {
+                        $qty = $updateItemsArr['qty'];
+                    } else {
+                        $qty = $buyRequest->getQty();
+                    }
+                    if (isset($updateItemsArr['start_date'])) {
+                        $start_date = $updateItemsArr['start_date'];
+                    } else {
+                        $start_date = $buyRequest->getStartDate();
+                    }
+                    if (isset($updateItemsArr['end_date'])) {
+                        $end_date = $updateItemsArr['end_date'];
+                    } else {
+                        $end_date = $buyRequest->getEndDate();
+                    }
 
                     if ((!isset($updateItemsArr['action']) || $updateItemsArr['action'] != 'remove') && !ITwebexperts_PPRWarehouse_Helper_Payperrentals_Data::isAvailable($item->getProduct(), $start_date, $end_date, $qty, $item->getStockId())) {
-						Mage::throwException('Product '.$item->getProduct()->getName().' is not available for that qty on the selected dates');
-						return;
-					}
-				}
-			}
-		}
-		return $this;
-	}
+                        Mage::throwException('Product ' . $item->getProduct()->getName() . ' is not available for that qty on the selected dates');
+                        return;
+                    }
+                }
+            }
+        }
+        return $this;
+    }
 
 
 }
